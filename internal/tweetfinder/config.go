3ad9b1a9af641520622cc2f74b97ce7e0d40ca00
khasanov.andrey <akhasanov@aicorner.org>
2023-11-14 21:32:44 +0200

    add finder pool

---
+++ internal/tweetfinder/config.go
@@ -0,0 +1,16 @@
+package tweetfinder
+
+import "github.com/kelseyhightower/envconfig"
+
+type ConfigPool struct {
+	XCreds          map[string]string `envconfig:"X_CREDS" required:"true"`
+	CookiesFilename string            `envconfig:"COOKIES_FILENAME" default:"cookies.json"`
+}
+
+func GetConfigPool() ConfigPool {
+	cfg := new(ConfigPool)
+	if err := envconfig.Process("", cfg); err != nil {
+		panic(err)
+	}
+	return *cfg
+}
---
+++ internal/tweetfinder/pool_fabric.go
@@ -0,0 +1,76 @@
+package tweetfinder
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+
+	twitterscraper "github.com/n0madic/twitter-scraper"
+
+	"github.com/lueurxax/crypto-tweet-sense/internal/log"
+)
+
+const startDelay = 15
+
+func NewPoolFabric(config ConfigPool, pkgKey string, logger log.Logger) (Finder, error) {
+	finders := make([]Finder, 0, len(config.XCreds))
+	delayManagerLogger := logger.WithField(pkgKey, "delay_manager")
+	finderLogger := logger.WithField(pkgKey, "finder")
+	for login, password := range config.XCreds {
+		filename := strings.Join([]string{login, config.CookiesFilename}, "_")
+		scraper := twitterscraper.New().WithDelay(startDelay).SetSearchMode(twitterscraper.SearchLatest)
+
+		var cookies []*http.Cookie
+
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			logger.Error(err)
+
+			if err = scraper.Login(login, password); err != nil {
+				return nil, err
+			}
+		}
+
+		if data != nil {
+			if err = json.Unmarshal(data, &cookies); err != nil {
+				logger.Error(err)
+
+				if err = scraper.Login(login, password); err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		if cookies != nil {
+			scraper.SetCookies(cookies)
+
+			if !scraper.IsLoggedIn() {
+				if err = scraper.Login(login, password); err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		cookies = scraper.GetCookies()
+
+		data, err = json.Marshal(cookies)
+		if err != nil {
+			return nil, err
+		}
+
+		if err = os.WriteFile(filename, data, 0644); err != nil {
+			return nil, err
+		}
+
+		delayManager := NewDelayManager(
+			func(seconds int64) { scraper.WithDelay(seconds) },
+			startDelay,
+			delayManagerLogger,
+		)
+
+		finders = append(finders, NewFinder(scraper, delayManager, finderLogger))
+	}
+
+	return NewPool(finders), nil
+}
