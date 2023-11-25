b8feddbe271492cb1f34fe7e5f3fcc820f11fa42
ANDREY KHASANOV <lueurxax@gmail.com>
2023-11-25 20:36:52 +0200

    clean old request limits code

--- internal/common/request_limits.go
+++ internal/common/request_limits.go
@@ -1,11 +1,8 @@
 package common
 
 import (
-	"strings"
 	"time"
 )
-
-const requestTimeFormat = "02T15:04:05"
 
 type RequestLimits struct {
 	WindowSeconds uint64
@@ -16,32 +13,6 @@ type RequestLimits struct {
 type Requests struct {
 	Data  []int32   `json:"data"`
 	Start time.Time `json:"start"`
-}
-
-type RequestTime time.Time
-
-func (t *RequestTime) MarshalJSON() ([]byte, error) {
-	return []byte(strings.Join([]string{"\"", time.Time(*t).UTC().Format(requestTimeFormat), "\""}, "")), nil
-}
-
-func (t *RequestTime) UnmarshalJSON(data []byte) error {
-	str := strings.Trim(string(data), "\"")
-
-	tt, err := time.Parse(requestTimeFormat, str)
-	if err == nil {
-		now := time.Now().UTC()
-
-		tt = tt.AddDate(now.Year(), int(now.Month()-1), 0)
-	} else {
-		tt, err = time.Parse(time.RFC3339Nano, str)
-		if err != nil {
-			return err
-		}
-	}
-
-	*t = RequestTime(tt)
-
-	return nil
 }
 
 type RequestLimitData struct {
