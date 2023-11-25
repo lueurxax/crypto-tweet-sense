7f1023b6d2b007b3a56fe5c51eee008022449241
ANDREY KHASANOV <lueurxax@gmail.com>
2023-11-25 19:31:30 +0200

    replaced old datetime format with new shorter format (#9)

--- internal/common/request_limits.go
+++ internal/common/request_limits.go
@@ -1,12 +1,42 @@
 package common
 
-import "time"
+import (
+	"strings"
+	"time"
+)
+
+const requestTimeFormat = "02T15:04:05"
 
 type RequestLimits struct {
-	WindowSeconds   uint64
-	CurrentRequests map[time.Time]struct{} `json:"CurrentRequests,omitempty"`
-	Requests        []time.Time            `json:"requests,omitempty"`
-	Threshold       uint64
+	WindowSeconds uint64
+	Requests      []RequestTime `json:"requests"`
+	Threshold     uint64
+}
+
+type RequestTime time.Time
+
+func (t *RequestTime) MarshalJSON() ([]byte, error) {
+	return []byte(strings.Join([]string{"\"", time.Time(*t).UTC().Format(requestTimeFormat), "\""}, "")), nil
+}
+
+func (t *RequestTime) UnmarshalJSON(data []byte) error {
+	str := strings.Trim(string(data), "\"")
+
+	tt, err := time.Parse(requestTimeFormat, str)
+	if err == nil {
+		now := time.Now().UTC()
+
+		tt = tt.AddDate(now.Year(), int(now.Month()-1), 0)
+	} else {
+		tt, err = time.Parse(time.RFC3339Nano, str)
+		if err != nil {
+			return err
+		}
+	}
+
+	*t = RequestTime(tt)
+
+	return nil
 }
 
 type RequestLimitData struct {
